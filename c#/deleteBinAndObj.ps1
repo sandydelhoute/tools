88da6dd779b1aa05c0ed104338ad0028f389c249
sandy <sandy.delhoute.extia@axa.fr>
2023-06-08 13:26:51 +0200

    feature(c#): update tools delete bin and obj folder

--- c#/deleteBinAndObj.ps1
+++ c#/deleteBinAndObj.ps1
@@ -1,7 +1,16 @@
-function clean_bin_obj()
-{
-	# DELETE ALL "BIN" and "OBJ" FOLDERS
-	get-childitem -Include bin -Recurse -force | Remove-Item -Force -Recurse
-	get-childitem -Include obj -Recurse -force | Remove-Item -Force -Recurse
+function clean_bin_obj() {
+    # DELETE ALL "BIN" and "OBJ" FOLDERS
+    get-childitem -Include bin -Recurse -force | Foreach-Object {
+        New-Object PSObject -Property @{
+            Directory = $_.FullName
+        }
+        Remove-Item -Path $_.FullName -Force -Recurse
+    }
+    get-childitem -Include obj -Recurse -force | Foreach-Object {
+        New-Object PSObject -Property @{
+            Directory = $_.FullName
+        }
+        Remove-Item -Path $_.FullName -Force -Recurse
+    }
 }
 clean_bin_obj
